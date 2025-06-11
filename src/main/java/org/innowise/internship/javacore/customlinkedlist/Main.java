36ad42f5ca4f19f12e6cf7ecd19c69518b2f5893
rumronya <rum.ronya@gmail.com>
2025-06-11 15:41:54 +0300

    clean main

--- src/main/java/org/innowise/internship/javacore/customlinkedlist/Main.java
+++ src/main/java/org/innowise/internship/javacore/customlinkedlist/Main.java
@@ -2,6 +2,6 @@ package org.innowise.internship.javacore.customlinkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
