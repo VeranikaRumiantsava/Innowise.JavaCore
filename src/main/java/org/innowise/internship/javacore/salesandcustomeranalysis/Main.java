5ee154e155b686d0f8d5f23a069b6a59b69abc7f
rumronya <rum.ronya@gmail.com>
2025-06-13 17:04:58 +0300

    stream api

---
+++ src/main/java/org/innowise/internship/javacore/salesandcustomeranalysis/Main.java
@@ -0,0 +1,6 @@
+package org.innowise.internship.javacore.salesandcustomeranalysis;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/org/innowise/internship/javacore/salesandcustomeranalysis/models/orders/Category.java
@@ -0,0 +1,5 @@
+package org.innowise.internship.javacore.salesandcustomeranalysis.models.orders;
+
+public enum Category {
+    ELECTRONICS, CLOTHING, BOOKS, HOME, BEAUTY, TOYS
+}
